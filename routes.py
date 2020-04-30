d563e96407b2a0369fdd1a4182ddfb436ad95dff
sanyam188 <sanyamjain.188@gmail.com>
2020-04-30 15:49:28 +0530

    sign up form added

--- routes.py
+++ routes.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 app=Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
-
-@app.route('/')
+client = MongoClient("mongodb://127.0.0.1:27017")  # host uri
+@app.route('/',methods=['GET','POST'])
 def fn():
+    if request.method=='POST':
+        mongo.db.col.insert({'name': request.form['name']})
     return render_template('index.html')
 @app.route('/registration')
 def reg():
