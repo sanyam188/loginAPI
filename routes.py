1a1e816da076b5f8fe93693351e798ef0a4b1d25
sanyam188 <sanyamjain.188@gmail.com>
2020-04-30 13:04:13 +0530

    Login route added

--- routes.py
+++ routes.py
@@ -1,4 +1,17 @@
-from flask import flask
+from flask import Flask,render_template
+from flask_pymongo import PyMongo
 app=Flask(__name__)
-@app.routes('/')
-render("Hello")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
+@app.route('/')
+def fn():
+    return render_template('index.html')
+@app.route('/registration')
+def reg():
+    return render_template('form.html')
+@app.route('/login')
+def log():
+    return render_template('log.html')
+if __name__=='__main__':
+    app.run(debug=True)
